a28d6bb32d4491484c9693e76f8c15c7733625bd
Silicasandwhich <43558271+Silicasandwhich@users.noreply.github.com>
2018-10-24 17:46:36 -0400

    added in logic

--- studentdb.py
+++ studentdb.py
@@ -12,14 +12,34 @@ class StudentDB(BoxLayout):
     student_list = ObjectProperty()
 
     def submit_students(self):
-        pass
-
+        # Get the student information
+        student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+        # Add to ListView
+        self.student_list.adapter.data.extend([student_name])
+        #Reset the ListView
+        self.student_list._trigger_reset_populate()
     def delete_students(self):
-        pass
-
+        #make sure student is selected
+        if self.student_list.adapter.selection:
+            #get text from item selected
+            selection = self.student_list.adapter.selection[0].text
+            #Remove the student
+            self.student_list.adapter.data.remove(selection)
+            #Reset the ListView
+            self.student_list._trigger_reset_populate()
     def replace_students(self):
-        pass
-
+        #make sure student is selected
+        if self.student_list.adapter.selection:
+            #get text from item selected
+            selection = self.student_list.adapter.selection[0].text
+            #Remove the student
+            self.student_list.adapter.data.remove(selection)
+            # Get student information
+            student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
+            # Add updated data to listView
+            self.student_list.adapter.data.extend([student_name])
+            #Reset the ListView
+            self.student_list._trigger_reset_populate()
 class StudentDBApp(App):
     def build(self):
         return StudentDB()
