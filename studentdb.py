2dcb36f11728fcccec7be036696bf3baee48c418
Silicasandwhich <43558271+Silicasandwhich@users.noreply.github.com>
2018-10-24 18:18:38 -0400

    pep8 compliant; .gitignore configured for pycharm

--- studentdb.py
+++ studentdb.py
@@ -3,8 +3,10 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.uix.listview import ListItemButton
 
+
 class StudentListButton(ListItemButton):
     pass
+
 
 class StudentDB(BoxLayout):
     first_name_text_input = ObjectProperty()
@@ -16,33 +18,38 @@ class StudentDB(BoxLayout):
         student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
         # Add to ListView
         self.student_list.adapter.data.extend([student_name])
-        #Reset the ListView
+        # Reset the ListView
+        # noinspection PyProtectedMember
         self.student_list._trigger_reset_populate()
+
     def delete_students(self):
-        #make sure student is selected
+        # make sure student is selected
         if self.student_list.adapter.selection:
-            #get text from item selected
+            # get text from item selected
             selection = self.student_list.adapter.selection[0].text
-            #Remove the student
+            # Remove the student
             self.student_list.adapter.data.remove(selection)
-            #Reset the ListView
+            # Reset the ListView
             self.student_list._trigger_reset_populate()
     def replace_students(self):
-        #make sure student is selected
+        # make sure student is selected
         if self.student_list.adapter.selection:
-            #get text from item selected
+            # get text from item selected
             selection = self.student_list.adapter.selection[0].text
-            #Remove the student
+            # Remove the student
             self.student_list.adapter.data.remove(selection)
             # Get student information
             student_name = self.first_name_text_input.text + " " + self.last_name_text_input.text
             # Add updated data to listView
             self.student_list.adapter.data.extend([student_name])
-            #Reset the ListView
+            # Reset the ListView
             self.student_list._trigger_reset_populate()
+
+
 class StudentDBApp(App):
     def build(self):
         return StudentDB()
+
 
 dbApp = StudentDBApp()
 dbApp.run()
